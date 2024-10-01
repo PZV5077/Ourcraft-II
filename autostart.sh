e9931d41007d481c05d7b1f5f6bcd578c4df2bb8
Eric Zhang <chiericzhang@outlook.com>
2024-10-01 12:08:53 +0000

    Backup: 01 Oct 24, 12:08 PM

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,6 @@
 echo Sleeping for 5s...
 sleep 5
-cd /workspaces/Ourcraft/server
-/workspaces/Ourcraft/backup.sh &
-/workspaces/Ourcraft/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 695900,695902 &
-java -Xmx12G -jar /workspaces/Ourcraft/server/fabric-server-mc.1.20.1-loader.0.16.5-launcher.1.0.1.jar nogui
+cd /workspaces/Ourcraft-II/server
+/workspaces/Ourcraft-II/backup.sh &
+/workspaces/Ourcraft-II/openfrp/frpc_linux_amd64 -u 790c9701e5bb7077d8936722dcb4c3cf -p 695900,695902,697498,697500,697501,697502,697503,697504 &
+java -Xmx12G -jar /workspaces/Ourcraft-II/server/fabric-server-mc.1.20.1-loader.0.16.5-launcher.1.0.1.jar nogui
